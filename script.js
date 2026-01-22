66dcc8c576291e7e69f4663e30a2a2b2db8d7bae
SirioRedGiant <Oiris1992@gmail.com>
2026-01-22 16:30:44 +0100

    prima stesura codice da verificare

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 //note Seleziono gli elementi del DOM
-const button = document.getElementById("generate-button");
+const button = document.getElementById("generate-btn");
 const list = document.getElementById("email-list");
+
+button.addEventListener("click", function () {
+  list.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((res) => {
+        const email = res.data.response;
+        list.innerHTML += `<li class="list-group-item">${email}</li>`;
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+});
